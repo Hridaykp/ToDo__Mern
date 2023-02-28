75f524e0f4b21824cd13faedb165a3b7ddf8a6ce
Hriday Kumar Pranit <hridaykp21@gmail.com>
2023-03-01 03:01:58 +0530

    cretae routes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,34 @@
 import express from "express";
+import mongoose from "mongoose";
+import Env from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import allRoutes from './routes/index.js'
+Env.config();
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.listen(PORT, ()=>{console.log(`Server is running on Port: ${PORT}`)})
+// middleware 
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+app.use('/api', allRoutes);
+
+
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Successfuly connected to MongoDB !!`)
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+app.listen(PORT, ()=>{
+    connectDB();
+    console.log(`Server is running on Port: ${PORT}`)
+})
