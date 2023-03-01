ae3c069aa60c5df5a4d5fa82943038888d20ff4d
Hriday Kumar Pranit <hridaykp21@gmail.com>
2023-03-01 18:10:48 +0530

    Create Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
+import { Routes,  Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home';
+import EditProfile from './pages/EditProfile';
+import Auth from './pages/Auth';
 
 function App() {
-  return (
-    <div className="App">
-        React
-    </div>
-  );
+    return (
+        <>
+            <Toaster 
+                position='top-right'
+                toastOptions={{
+                    style:{
+                        fontSize:'1.7rem'
+                    }
+                }}
+            ></Toaster>
+            <Routes>
+                <Route exact path='/' element={< Home />} />
+                <Route exact path='/edit-profile' element={< EditProfile />} />
+                <Route exact path='/auth' element={< Auth />} />
+
+            </Routes>
+        </>
+    );
 }
 
 export default App;
