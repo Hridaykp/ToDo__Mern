9052c43940124f666eca066c39605a24f29e3684
Hriday Kumar Pranit <hridaykp21@gmail.com>
2023-03-02 01:21:11 +0530

    handling auth page (if user logged in then no redirect to auth page without logging out)

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import Login from '../components/auth/Login';
 import Register from '../components/auth/Register';
 import Layout from '../components/Layout';
 import styles from './Auth.module.scss';
-// import useAuth from '../hooks/useAuth';
-// import { useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Auth() {
-    // const { auth } = useAuth();
-    // const navigate = useNavigate();
-    // useEffect(()=>{
-    //     if(auth){ navigate('/')}
-    // }, [auth, navigate]);
+    const { auth } = useAuth();
+    const navigate = useNavigate();
+    useEffect(()=>{
+        if(auth){ navigate('/')}
+    }, [auth, navigate]);
 
     return (
         <Layout>
